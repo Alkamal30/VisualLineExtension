84859b4107fe53978ecdbd504a2969d5256bdae9
Alkamal30 <yurevich.vlad3005@gmail.com>
2024-07-30 18:03:26 +0300

    Add visual line adornment

---
+++ VisualLineAdornment.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace VisualLineExtension
+{
+    internal sealed class VisualLineAdornment
+    {
+        private const double LeftMargin = 140d;
+        private const double LineThickness = 1d;
+        private const double LinePositionOffset = 2.5d;
+        private static readonly Color LineColor = Color.FromRgb(46, 46, 46);
+
+        private readonly IWpfTextView _textView;
+        private readonly IAdornmentLayer _adornmentLayer;
+        private readonly Line _verticalLine;
+
+        public VisualLineAdornment(IWpfTextView view)
+        {
+            _textView = view ?? throw new ArgumentNullException("view");
+
+            _verticalLine = new Line
+            {
+                Stroke = BuildColorBrush(),
+                StrokeThickness = LineThickness,
+            };
+
+            _adornmentLayer = view.GetAdornmentLayer(nameof(VisualLineAdornment));
+
+            _textView.LayoutChanged += OnSizeChanged;
+        }
+
+        private static Brush BuildColorBrush()
+        {
+            Brush colorBrush = new SolidColorBrush(LineColor);
+            colorBrush.Freeze();
+
+            return colorBrush;
+        }
+
+        private void OnSizeChanged(object sender, EventArgs e)
+        {
+            _adornmentLayer.RemoveAllAdornments();
+
+            _verticalLine.Y2 = _textView.ViewportHeight;
+
+            Canvas.SetLeft(_verticalLine, (LeftMargin - 1) * _textView.FormattedLineSource.ColumnWidth + LinePositionOffset);
+            Canvas.SetTop(_verticalLine, _textView.ViewportTop);
+
+            _adornmentLayer.AddAdornment(AdornmentPositioningBehavior.ViewportRelative, null, null, _verticalLine, null);
+        }
+    }
+}
---
+++ VisualLineAdornmentTextViewCreationListener.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace VisualLineExtension
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class VisualLineAdornmentTextViewCreationListener : IWpfTextViewCreationListener
+    {
+#pragma warning disable IDE0051
+
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name(nameof(VisualLineAdornment))]
+        [Order(Before = PredefinedAdornmentLayers.Text)]
+        private readonly AdornmentLayerDefinition editorAdornmentLayer;
+
+#pragma warning restore IDE0051
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            new VisualLineAdornment(textView);
+        }
+    }
+}
